1e5190a3fac5b7a60d891ae2c8c28a02c87bc552
Ben Governali <bgovern93@gmail.com>
2019-07-24 15:14:55 -0400

    login functional, bearer stored in react state

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express"),
   path = require("path"),
   cookieParser = require("cookie-parser"),
-  passport = require("passport"),
   session = require("express-session"),
   FileStore = require("session-file-store")(session),
   logger = require("morgan");
@@ -13,7 +12,7 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(process.env["SESSION_SECRET"]));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
@@ -21,20 +20,12 @@ app.use(
     secret: process.env["SESSION_SECRET"],
     resave: true,
     saveUninitialized: true,
-    is_logged_in: false
+    is_logged_in: false,
+    cookie: {
+      secure: false
+    }
   })
 );
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
 
 const indexRouter = require("./routes/index"),
   homeRouter = require("./routes/home"),
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,4 @@
 const express = require("express"),
-  querystring = require("querystring"),
-  request = require("request"),
-  session = require("express-session"),
   passport = require("passport"),
   SpotifyStrategy = require("passport-spotify").Strategy,
   router = express.Router();
@@ -13,63 +10,29 @@ const scope = "user-library-read";
 const CLIENT_ID = process.env["CLIENT_ID"];
 const CLIENT_SECRET = process.env["CLIENT_SECRET"];
 
-//declare state key
-const stateKey = "spotify_auth_state";
+router.get("/spotify", function(req, res) {
+  var scopes = "user-library-read";
+  res.redirect(
+    "https://accounts.spotify.com/authorize" +
+      "?response_type=code" +
+      "&client_id=" +
+      CLIENT_ID +
+      (scopes ? "&scope=" + encodeURIComponent(scopes) : "") +
+      "&redirect_uri=" +
+      encodeURIComponent("http://localhost:3000/auth/spotify/callback/")
+  );
+});
 
-//configure passport strategy
-passport.use(
-  new SpotifyStrategy(
-    {
-      clientID: CLIENT_ID,
-      clientSecret: CLIENT_SECRET,
-      callbackURL: `http://localhost:3000/auth/spotify/callback/`
-    },
-    function(accessToken, refreshToken, expires_in, profile, done) {
-      process.nextTick(function() {
-        //req.session.user = profile.id;
-        //req.session.bearer = accessToken;
-        //req.session.isLoggedIn = true;
-        bearer = accessToken;
-        user = profile.id;
-        return done(null, bearer, user);
-      });
-    }
-  )
-);
-
-//Handle login through Spotify
-router.get(
-  "/spotify",
-  passport.authenticate("spotify", {
-    scope: scope,
-    showDialog: true
-  }),
-  function(req, res) {
-    // The request will be redirected to spotify for authentication, so this
-    // function will not be called.
-  }
-);
-
-//Handle redirect back to front end and set session variables
-router.get(
-  "/spotify/callback/",
-  passport.authenticate("spotify", { failureRedirect: "/" }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    req.session.bearer = req.user;
-    req.session.user = req.authInfo;
-
-    console.log("req user", req.user);
-    console.log("req auth info", req.authInfo);
-
-    res.redirect("http://localhost:3001/home");
-  }
-);
+router.get("/spotify/callback", async function(req, res) {
+  console.log(req.query.code);
+  //req.session.bearer = req.query.code;
+  //req.session.isLoggedIn = true;
+  //await req.session.save(req.session.bearer, req.session.isLoggedIn);
+  res.redirect(`http://localhost:3001/?bearer=${req.query.code}`);
+});
 
 router.get("/scan", async function(req, res, next) {
-  console.log("req user", req.user);
   console.log("req session", req.session);
-  console.log("passport session", passport.session);
 });
 
 module.exports = router;
