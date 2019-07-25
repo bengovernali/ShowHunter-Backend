97f04151c914dddebdd0253eeb8346a393bd419e
Ben Governali <bgovern93@gmail.com>
2019-07-25 16:33:02 -0400

    array of artist ids is filtered.

--- routes/home.js
+++ routes/home.js
@@ -4,13 +4,30 @@ const express = require("express"),
 
 const router = express.Router();
 
-async function mapArtistIds(data, array) {
+function mapArtistIds(data, array) {
   return data.map(item => array.push(item.track.album.artists[0].id));
+}
+
+function getRelatedArtists(id, bearer) {
+  const authOptions2 = {
+    url: `https://api.spotify.com/v1/artists/${id}/related-artists`,
+    headers: { Authorization: `Bearer ${bearer}` },
+    json: true
+  };
+
+  request.get(authOptions2, (error, response, body) => {
+    return body;
+  });
+}
+
+function mapRelatedArtists(data, array) {
+  return data.map(item => array.push(item.name));
 }
 
 //request data from spotify
 router.get("/scan/:bearer", async function(req, res, next) {
   let user_artists = [];
+  let filteredIds = [];
   let related_artists = [];
 
   const bearer = req.params.bearer;
@@ -20,27 +37,18 @@ router.get("/scan/:bearer", async function(req, res, next) {
     json: true
   };
 
+  //request top 50 tracks from library
   await request.get(authOptions, async (error, response, body) => {
     if (error) throw new Error(error);
 
     //create an array of artist ids from the first 50 tracks in a users spotify library
     await mapArtistIds(body.items, user_artists);
-    console.log(user_artists);
 
-    //create an array of related artists
-    /*
-    user_artists.forEach(id => {
-      const authOptions2 = {
-        url: `https://api.spotify.com/v1/artists/${id}/related-artists`,
-        headers: { Authorization: `Bearer ${bearer}` },
-        json: true
-      };
-
-      request.get(authOptions2, (error, response, body) => {
-        body.artists.map(artist => related_artists.push(artist.name));
-      });
+    //filter out duplicate artist ids
+    const filteredIds = user_artists.filter((item, index) => {
+      return user_artists.indexOf(item) === index;
     });
-    */
+    console.log(filteredIds);
   });
 });
 
