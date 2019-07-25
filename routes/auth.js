cf3b6e038e19921645a6712cd2c3195fa9dc38de
Ben Governali <bgovern93@gmail.com>
2019-07-25 14:32:21 -0400

    now receiving auth token and pulling artist ids fro mthe top 50 tracks in a users library

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,16 @@
 const express = require("express"),
-  passport = require("passport"),
-  SpotifyStrategy = require("passport-spotify").Strategy,
+  request = require("request"),
   router = express.Router();
 
 require("dotenv").config();
 
-const scope = "user-library-read";
-
 const CLIENT_ID = process.env["CLIENT_ID"];
 const CLIENT_SECRET = process.env["CLIENT_SECRET"];
 
+//handle redirect to spotify login screen
 router.get("/spotify", function(req, res) {
-  var scopes = "user-library-read";
+  const scopes =
+    "user-read-private user-read-birthdate user-read-email playlist-read-private user-library-read user-library-modify user-top-read playlist-read-collaborative playlist-modify-public playlist-modify-private user-follow-read user-follow-modify user-read-playback-state user-read-currently-playing user-modify-playback-state user-read-recently-played";
   res.redirect(
     "https://accounts.spotify.com/authorize" +
       "?response_type=code" +
@@ -23,16 +22,34 @@ router.get("/spotify", function(req, res) {
   );
 });
 
+//upon login submission, request auth token and redirect to fronend with token
 router.get("/spotify/callback", async function(req, res) {
-  console.log(req.query.code);
-  //req.session.bearer = req.query.code;
-  //req.session.isLoggedIn = true;
-  //await req.session.save(req.session.bearer, req.session.isLoggedIn);
-  res.redirect(`http://localhost:3001/?bearer=${req.query.code}`);
-});
+  const code = req.query.code;
 
-router.get("/scan", async function(req, res, next) {
-  console.log("req session", req.session);
+  const authString = `${CLIENT_ID} + ":" + ${CLIENT_SECRET}`;
+
+  const options = {
+    method: "POST",
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      Authorization:
+        "Basic " +
+        new Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64")
+    },
+    form: {
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: "http://localhost:3000/auth/spotify/callback/"
+    },
+    json: true
+  };
+
+  await request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    const token = body.access_token;
+    //const refresh = body.refresh_token;
+    res.redirect(`http://localhost:3001/?bearer=${token}`);
+  });
 });
 
 module.exports = router;
--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,47 @@
 const express = require("express"),
-  session = require("express-session");
+  request = require("request"),
+  fetch = require("node-fetch");
 
 const router = express.Router();
 
-router.get("/scan", async function(req, res, next) {
-  console.log(req.session);
+async function mapArtistIds(data, array) {
+  return data.map(item => array.push(item.track.album.artists[0].id));
+}
+
+//request data from spotify
+router.get("/scan/:bearer", async function(req, res, next) {
+  let user_artists = [];
+  let related_artists = [];
+
+  const bearer = req.params.bearer;
+  const authOptions = {
+    url: `https://api.spotify.com/v1/me/tracks?&limit=50`,
+    headers: { Authorization: `Bearer ${bearer}` },
+    json: true
+  };
+
+  await request.get(authOptions, async (error, response, body) => {
+    if (error) throw new Error(error);
+
+    //create an array of artist ids from the first 50 tracks in a users spotify library
+    await mapArtistIds(body.items, user_artists);
+    console.log(user_artists);
+
+    //create an array of related artists
+    /*
+    user_artists.forEach(id => {
+      const authOptions2 = {
+        url: `https://api.spotify.com/v1/artists/${id}/related-artists`,
+        headers: { Authorization: `Bearer ${bearer}` },
+        json: true
+      };
+
+      request.get(authOptions2, (error, response, body) => {
+        body.artists.map(artist => related_artists.push(artist.name));
+      });
+    });
+    */
+  });
 });
 
 module.exports = router;
